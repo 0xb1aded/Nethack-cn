1b7f5e0b9db2e729b49b4c91d847931d26fa3d7c
Francium-223 <wizardofyendor@outlook.com>
2026-05-27 20:39:09 +0800

    Update src/hacklib.c
    Co-authored-by: StackC00ki3 <60530949+StackC00ki3@users.noreply.github.com>

--- src/hacklib.c
+++ src/hacklib.c
@@ -615,8 +615,6 @@ ordin(int n)               /* note: should be non-negative */
 {
     int dd = n % 10;
     return ""; /*危险*/
-    return (dd == 0 || dd > 3 || (n % 100) / 10 == 1) ? "th"
-               : (dd == 1) ? "st" : (dd == 2) ? "nd" : "rd";
 }
 
 DISABLE_WARNING_FORMAT_NONLITERAL  /* one compiler complains about
