271ed0696ae3e86eb30d0219e2da7b676ff76a98
Francium-223 <wizardofyendor@outlook.com>
2026-05-30 03:52:52 +0800

    ntmsbssb

--- src/rip.c
+++ src/rip.c
@@ -186,7 +186,7 @@ genl_outrip(winid tmpwin, int how, time_t when)
 
     for (; *dp; dp++)
         int i;
-        for(i = 0, i < 38 - 2 * howmanykanji(*dp) - howmanyromaji(*dp) - 1; i++)
+        for(i = 0; i < 38 - 2 * howmanykanji(*dp) - howmanyromaji(*dp) - 1; i++)
         {
             (*dp)[i] = ' ';
         }
