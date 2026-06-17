769e039a89ec53984cf2b36b069f3f911f932d84
Francium-223 <wizardofyendor@outlook.com>
2026-06-17 20:01:00 +0800

    updated findkanjifiles.py

--- .github/workflows/findkanjifiles.py
+++ .github/workflows/findkanjifiles.py
@@ -12,19 +12,24 @@ def getfiles(path):
         except:
             e.append(d)
     return e
-files = getfiles('..\\NetHack-cn')
+files = getfiles('..\\..')
 kanjifiles = []
 common = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>? \n\r\t\b\f"
 for i in files:
-	try:
-		f = open(i, 'r').read()
-		for j in f:
-			if not j in common:
-				kanjifiles.append(i)
-				break
-	except:
-		continue
-print(kanjifiles)
+    if '.git\\' in i or 'binary\\' in i:
+        continue
+    else:
+        try:
+            f = open(i, 'r').read()
+            for j in f:
+                if not j in common:
+                    kanjifiles.append(i)
+                    break
+        except:
+            continue
+with open('..\\..\\kanjifiles.txt', 'w') as f:
+    for i in kanjifiles:
+        print(i[6:], file = f)
 '''
 for i in files:
     try:
