7929fda86eba27f2f249e42f242a0819b4d26877
Francium-223 <wizardofyendor@outlook.com>
2026-08-27 20:34:14 +0800

    Fix pdcursesmod version mismatch causing LNK2001 in VS build
    Align pdcursesmod downloads to 4.5.3 (matching submodules/pdcursesmod)
    and resolve the merge conflict markers in sys/windows/fetch.cmd.

--- sys/windows/fetch.cmd
+++ sys/windows/fetch.cmd
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-@echo off
-setlocal enabledelayedexpansion
-if not exist lib\* mkdir lib
-
-if [%1] == [lua] (
-   set LUA_VERSION=5.4.8
-   set LUASRC=../lib/lua
-   set CURLLUASRC=http://www.lua.org/ftp/lua-!LUA_VERSION!.tar.gz
-   set CURLLUADST=lua-!LUA_VERSION!.tar.gz
-   if NOT exist lib\lua-!LUA_VERSION!\src\lua.h (
-       cd lib
-       curl -L !CURLLUASRC! -o !CURLLUADST!
-       tar -xvf !CURLLUADST!
-       cd ..
-   )
-   echo Lua placed in lib\lua-!LUA_VERSION!
-)
-
-if [%1] == [pdcursesmod] (
-   set PDCVERSION=4.4.0
-   set CURLPDCSRC=https://github.com/Bill-Gray/PDCursesMod/archive/refs/tags/v!PDCVERSION!.zip
-   set CURLPDCDST=pdcursesmod.zip
-   if NOT exist lib\pdcursesmod\curses.h (
-       cd lib
-       curl -L !CURLPDCSRC! -o !CURLPDCDST!
-       mkdir pdcursesmod
-       tar -C pdcursesmod --strip-components=1 -xvf !CURLPDCDST!
-       cd ..
-   )
-)
-=======
 @echo on
 
 if exist %cd%\..\sys\windows\Makefile.nmake set LIBDIR=..\lib
@@ -41,7 +9,7 @@ set LUASRC=%LIBDIR%/lua
 set CURLLUASRC=http://www.lua.org/ftp/lua-%LUA_VERSION%.tar.gz
 set CURLLUADST=lua-%LUA_VERSION%.tar.gz
 
-set PDCVERSION=4.5.4
+set PDCVERSION=4.5.3
 set CURLPDCSRC=https://github.com/Bill-Gray/PDCursesMod/archive/refs/tags/v%PDCVERSION%.zip
 set CURLPDCDST=pdcursesmod.zip
 
@@ -71,4 +39,3 @@ if [%1] == [pdcursesmod] (
 )
 
 :done
->>>>>>> upstream/NetHack-5.0
