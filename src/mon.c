7810414cac065741d78952c97451853a766d33ff
Francium-223 <wizardofyendor@outlook.com>
2026-09-02 07:01:21 +0800

    hideunder: 一个foo藏在bar下

--- src/mon.c
+++ src/mon.c
@@ -4800,7 +4800,7 @@ hideunder(struct monst *mtmp)
             if (!locomo)
                 locomo = locomotion(mtmp->data, "藏");
             set_msg_xy(mtmp->mx, mtmp->my); /* pline() will reset this */
-            You_see("%s%s在%s下.", seenmon, locomo, seenobj);
+            You_see("一%s%s%s在%s下.", mon_quantifier(mtmp), seenmon, locomo, seenobj); //修改语序:You_see("%s%s在%s下.", seenmon, locomo, seenobj);
             iflags.last_msg = PLNMSG_HIDE_UNDER;
             gl.last_hider = mtmp->m_id;
         }
